0460cb0e268c6785c9138ad7ef7b53d8bffe49ab
juancprzs <jc.perez13@uniandes.edu.co>
2020-06-16 16:28:35 -0500

    fix path to last cjheckpoint

--- main_clustr.py
+++ main_clustr.py
@@ -143,7 +143,7 @@ def main():
         standard_epsilons=STANDARD_EPSILONS, alpha_step=ALPHA_STEP, L=args.L, 
         seed=args.seed, normalize_probs=not args.not_normalize,
         restarts=args.restarts, attack_iters=args.iterations,
-        evaluate_ckpt=f'checkpoint_{epoch}.pth'
+        evaluate_ckpt=os.path.join(args.checkpoint, 'checkpoint.pth')
     )
 
 if __name__ == '__main__':
